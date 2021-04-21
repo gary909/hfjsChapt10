5f7ee49155252d1e33b702f33f7828d79f53025d
gary <gary.white@lodgit.com>
2021-04-21 16:34:36 +0200

    fly code

---
+++ fly.js
@@ -0,0 +1,4 @@
+var passengers = [
+    { name: "Jane Doloop", paid: true }, { name: "Dr. Evel", paid: true }, 
+    { name: "Sue Property", paid: false }, { name: "John Funcall", paid: true }  
+];
